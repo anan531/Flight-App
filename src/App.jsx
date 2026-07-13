af45654d383308e63a80827bf6b298ccb26c24dd
anan531 <ananyat612@gmail.com>
2026-07-13 14:14:31 +0530

    add success

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddFlight from './components/AddFlight'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <AddFlight/>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<AddFlight/>}/>
+
+
+     </Routes>
+     
+     </BrowserRouter>
     </>
   )
 }
